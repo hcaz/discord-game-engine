8733fab15acf19b15b7eb67ac35ddb7ed5f8afc1
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2018-10-19 20:57:45 +0100

    More help messages

--- bot.js
+++ bot.js
@@ -15,9 +15,18 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('game'):
         client.send_typing(message.channel)
-        intent = message.content.strip('duct ').split()
+        intent = message.content[5:].split()
         if intent[0].startswith('start'):
             await client.send_message(message.channel, 'Starting game - ' + intent[1])
+        elif intent[0].startswith('types'):
+            embed=discord.Embed(title="Game Types", description="Below is a list of game types available", color=0xa709ff)
+            embed.set_author(name="Game Engine", url="https://github.com/hcaz/discord-game-engine", icon_url="https://api.adorable.io/avatars/285/" + message.author.name)
+            embed.set_thumbnail(url="https://image.flaticon.com/icons/svg/148/148769.svg")
+            embed.add_field(name="tie-tac-toe", value="2 players", inline=False)
+            embed.add_field(name="connect-four", value="2 players", inline=False)
+            embed.add_field(name="chess", value="2 players", inline=True)
+            embed.set_footer(text="For more help, visit the discord!")
+            await client.send_message(message.channel, embed=embed)
         else:
             embed=discord.Embed(title="Help", description="This is a list of all commands available from this bot", color=0xa709ff)
             embed.set_author(name="Game Engine", url="https://github.com/hcaz/discord-game-engine", icon_url="https://api.adorable.io/avatars/285/" + message.author.name)
